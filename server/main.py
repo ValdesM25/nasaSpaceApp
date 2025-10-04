0a2dc00348c0ed7e3680c1c263f301ff64f95c89
Leonardo Antonio Tovíaz López <toviaz.leonardo@gmail.com>
2025-10-04 11:07:18 -0600

    server sirve login, falta funcionalidad hardcodeada y diseño

--- server/main.py
+++ server/main.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# login
 @app.route("/")
 def home():
-    return render_template("index.html", prueba="")
+    return render_template("login.html", prueba="")
 
 
 # main
