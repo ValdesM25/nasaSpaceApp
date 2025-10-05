565f2277d013620a5f7624a6753ae5d1f779deda
Fernando <ferlr33@outlook.com>
2025-10-05 13:45:43 -0600

    Fixed pagina prediccion

--- server/static/predict.js
+++ server/static/predict.js
@@ -1,18 +1,4 @@
 document.getElementById('sendRequestBtn').addEventListener('click', () => {
-    fetch('/dfres', {
-        method: 'GET',  // o 'GET', según tu endpoint
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        
-    })
-    .then(response => response.text())
-    .then(data => {
-        //console.log('Response:', data);
-        alert(data);  // opcional, puedes mostrar un mensaje bonito
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('Something went wrong.');
-    });
+    // Redirige al endpoint /results
+    window.location.href = '/results';
 });
