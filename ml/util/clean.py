c0492640e6dc94abf037e075144b62a1ec4e5769
Fernando <ferlr33@outlook.com>
2025-10-05 05:14:34 -0600

    Pipeline, entrenamiento, api

--- ml/util/clean.py
+++ ml/util/clean.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import numpy as np
-from columnas import *
+from ml.util.columns import *
 
 # Generate error columns
 def generate_errors(df, cols, drop=True):
@@ -42,17 +42,17 @@ def preprocess(df, used_columns, cols_dict, row_query=None):
 used_columns = ['category', 'transit_period', 'transit_duration', 'transit_depth', 'transit_depth', 'planet_radius', 'star_temperature', 'star_radius', 'star_surface_gravity', 'star_brightness']
 
 # Kepler
-kepler_raw = pd.read_csv('ml/datasets/kepler_objects_of_interest_koi.csv')
+kepler_raw = pd.read_csv('ml/datasets/raw/kepler_objects_of_interest_koi.csv')
 kepler_used_columns = ['koi_disposition', 'koi_period', 'koi_duration', 'koi_depth', 'koi_prad', 'koi_steff', 'koi_srad', 'koi_slogg', 'koi_kepmag']
 kepler = preprocess(kepler_raw, used_columns, kepler_cols_dict)
 
 # Tess
-tess_raw = pd.read_csv('ml/datasets/tess_objects_of_interest_toi.csv')
+tess_raw = pd.read_csv('ml/datasets/raw/tess_objects_of_interest_toi.csv')
 tess_used_columns = ['tfopwg_disp', 'pl_orbper', 'pl_trandurh', 'pl_trandep', 'pl_rade', 'st_teff', 'st_rad', 'st_logg', 'st_tmag']
 tess = preprocess(tess_raw, used_columns, tess_cols_dict)
 
 # K2
-k2_raw = pd.read_csv('ml/datasets/k2_planets_and_candidates.csv')
+k2_raw = pd.read_csv('ml/datasets/raw/k2_planets_and_candidates.csv')
 k2_raw['pl_trandep'] *= 10000
 k2_raw['pl_trandeperr1'] *= 10000
 k2_raw['pl_trandeperr2'] *= 10000
@@ -61,6 +61,6 @@ k2_used_columns = ['disposition', 'pl_orbper', 'pl_trandur', 'pl_trandep', 'pl_r
 k2 = preprocess(k2_raw, used_columns, k2_cols_dict)
 
 # Save datasets
-kepler.to_csv('ml/datasets/kepler.csv', index=False)
-tess.to_csv('ml/datasets/tess.csv', index=False)
-k2.to_csv('ml/datasets/k2.csv', index=False)
+kepler.to_csv('ml/datasets/cleaned/kepler.csv', index=False)
+tess.to_csv('ml/datasets/cleaned/tess.csv', index=False)
+k2.to_csv('ml/datasets/cleaned/cleaned/k2.csv', index=False)
